e4c48e98a99e434578b369ff45c1cb63d26dbc33
alfredo7792 <alfredo7791@gmail.com>
2024-08-19 01:08:26 -0500

    add .env

--- app/models/db_models.py
+++ app/models/db_models.py
@@ -6,10 +6,10 @@ load_dotenv()
 
 def get_db_connection():
     return mysql.connector.connect(
-        host=os.getenv('DB_HOST', 'localhost'),
-        user=os.getenv('DB_USERNAME', 'root'),
-        password=os.getenv('DB_PASSWORD', '12345678'),
-        database=os.getenv('DB_DATABASE', 'bd_ecommerce_shoes'),
+        host=os.getenv('DB_HOST'),
+        user=os.getenv('DB_USERNAME'),
+        password=os.getenv('DB_PASSWORD'),
+        database=os.getenv('DB_DATABASE'),
         charset='utf8mb4'
     )
 
